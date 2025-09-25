592eb598acf7af133370fdb39096f793bc6d5858
tsmialek <tsmialek.1@gmail.com>
2025-09-25 20:42:17 +0200

    feat: integrate SYSTEM_PROMPT into content generation for improved AI responses

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 MAX_FILE_READ_CHARS = 10000
 PYTHON_RUN_TIMEOUT = 30
+SYSTEM_PROMPT = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import SYSTEM_PROMPT
 
 
 def main():
@@ -27,11 +28,11 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    generateContent()
-
     def generateContent():
         model_response = client.models.generate_content(
-            model="gemini-2.0-flash-001", contents=messages
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT),
         )
 
         if verbose:
@@ -43,6 +44,8 @@ def main():
             print(
                 f"Response tokens: {model_response.usage_metadata.candidates_token_count}"
             )
+
+    generateContent()
 
 
 if __name__ == "__main__":
